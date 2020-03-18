337e6fa0391d02c690705aa66f8e57959e56465e
k.kravchik <k.kravchik@webvork.com>
2020-03-18 14:59:47 +0300

    final

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,3 @@ from flask_cors import CORS, cross_origin
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
-    # socketio.run(app, host='0.0.0.0', port=80, debug=True)  #cors_allowed_origins="*", 
